8edac00404d22695d7348ec48759e16b9fb457f6
Mohammed9010 <laufderhar@student.umgc.edu>
2026-09-04 20:09:59 +0100

    Configure Next.js for Cloudflare Workers

--- next.config.ts
+++ next.config.ts
@@ -23,3 +23,5 @@ const nextConfig: NextConfig = {
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 export default withNextIntl(nextConfig);
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
